1c1adeec33f145d64d37b69a992cfb9d3310f594
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-08-30 03:07:04 +0530

    Current Status: Everthing is working perfectly fine. Todo: Add card in the page root directory (already added comments) Good night

--- engitrack/src/app/page.js
+++ engitrack/src/app/page.js
@@ -1,28 +1,31 @@
-import { Roboto } from 'next/font/google'
-import Image from 'next/image';
+import { Roboto } from "next/font/google";
+import Image from "next/image";
 import Link from "next/link";
 const roboto = Roboto({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-})
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
-  title: 'Engi Track',
-  description: 'Engi Track is a leading online platform in India that connects engineering students with internships and offers services like resume building and career counseling.',
-  openGraph:{
-    images:[
+  title: "Engi Track",
+  description:
+    "Engi Track is a leading online platform in India that connects engineering students with internships and offers services like resume building and career counseling.",
+  openGraph: {
+    images: [
       {
-        url: '/engitrack/public/vercel.svg',
-        alt: 'Engi Track',
-      }
-    ]
-  }
-}
+        url: "/engitrack/public/vercel.svg",
+        alt: "Engi Track",
+      },
+    ],
+  },
+};
 
 export default function HomePage() {
   return (
-    <div className={`flex flex-col items-center justify-start min-h-screen text-center px-4 py-8 pt-16 sm:pt-28 ${roboto.className}`}>
+    <div
+      className={`flex flex-col items-center justify-start min-h-screen text-center px-4 py-8 pt-16 sm:pt-28 ${roboto.className}`}
+    >
       <div className="max-w-4xl mx-auto">
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-normal leading-tight">
           <span className="block mb-2 sm:mb-4">Connect. Learn. Grow.</span>
@@ -32,58 +35,80 @@ export default function HomePage() {
       </div>
 
       <p className="mt-8 sm:mt-12 lg:mt-16">
-        Unlock endless opportunities in the world of corporate <br/> with Engitrack's comprehensive internship program.
+        Unlock endless opportunities in the world of corporate <br /> with
+        Engitrack's comprehensive internship program.
       </p>
 
       {/* add a button here */}
       <div>
-        <Link href= "/join">
-        <button className="bg-primary text-primary-foreground hover:bg-primary/90 h-12 
+        <Link href="/join">
+          <button
+            className="bg-primary text-primary-foreground hover:bg-primary/90 h-12 
         px-6 mt-8 rounded-md text-sm font-medium ring-offset-background 
         transition-colors focus-visible:outline-none focus-visible:ring-2 
         focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none 
-        disabled:opacity-50">Apply Now</button>
+        disabled:opacity-50"
+          >
+            Apply Now
+          </button>
         </Link>
       </div>
 
       {/* Another button here */}
       <div>
-        <Link href= "/explore">
-        <button className="bg-transparent text-black hover:text-gray-700 h-12 
+        <Link href="/explore">
+          <button
+            className="bg-transparent text-black hover:text-gray-700 h-12 
         px-6 mt-8 rounded-md text-l font-medium ring-offset-background 
         transition-colors focus-visible:outline-none focus-visible:ring-2 
         focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none 
-        disabled:opacity-50">Explore</button>
+        disabled:opacity-50"
+          >
+            Explore
+          </button>
         </Link>
       </div>
 
       <div className="max-w-4xl mx-auto pt-16 flex flex-col lg:flex-row items-center lg:items-start">
-      <div className="lg:w-1/2 text-center lg:text-left">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-normal leading-tight">
-          <span className="block mb-2 sm:mb-4">Empowering Students</span>
-          <span className="block mb-2 sm:mb-4">Through Opportunities</span>
-        </h1>
-        <p className="mt-8 sm:mt-12 lg:mt-16">
-          Engitrack is a leading online platform in India that
-          <br />
-          connects engineering students with internships and offers
-          <br />
-          services like resume building and career counseling.
-        </p>
+        <div className="lg:w-1/2 text-center lg:text-left">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-normal leading-tight">
+            <span className="block mb-2 sm:mb-4">Empowering Students</span>
+            <span className="block mb-2 sm:mb-4">Through Opportunities</span>
+          </h1>
+          <p className="mt-8 sm:mt-12 lg:mt-16">
+            Engitrack is a leading online platform in India that
+            <br />
+            connects engineering students with internships and offers
+            <br />
+            services like resume building and career counseling.
+          </p>
+        </div>
+        <div className="lg:w-1/2 mt-8 lg:mt-0 lg:ml-12 flex justify-center lg:justify-end relative">
+          <Image
+            src="https://media.istockphoto.com/id/1587604256/photo/portrait-lawyer-and-black-woman-with-tablet-smile-and-happy-in-office-workplace-african.jpg?b=1&s=612x612&w=0&k=20&c=4K6Bf1I6OnKzJmLaqVArPqIBTZ3VnDGJfx6hohXb1pg="
+            alt="hero"
+            width={500}
+            height={500}
+            className="rounded-lg"
+          />
+          <div className="absolute bottom-[-30px] lg:bottom-[-40px] left-1/2 transform -translate-x-1/2 bg-black text-white px-6 py-3 rounded-md shadow-lg text-lg lg:text-xl">
+            Trusted by 150+ Students
+          </div>
+        </div>
       </div>
-      <div className="lg:w-1/2 mt-8 lg:mt-0 lg:ml-12 flex justify-center lg:justify-end relative">
-  <Image
-    src="https://media.istockphoto.com/id/1587604256/photo/portrait-lawyer-and-black-woman-with-tablet-smile-and-happy-in-office-workplace-african.jpg?b=1&s=612x612&w=0&k=20&c=4K6Bf1I6OnKzJmLaqVArPqIBTZ3VnDGJfx6hohXb1pg="
-    alt="hero"
-    width={500}
-    height={500}
-    className="rounded-lg"
-  />
-  <div className="absolute bottom-[-30px] lg:bottom-[-40px] left-1/2 transform -translate-x-1/2 bg-black text-white px-6 py-3 rounded-md shadow-lg text-lg lg:text-xl">
-    Trusted by 150+ Students
-  </div>
-</div>
-    </div>
+
+      <div className="max-w-4xl mx-auto pt-40">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-normal leading-tight">
+          <span className="block mb-2 sm:mb-4">Internship Connection</span>
+          <span className="block mb-2 sm:mb-4">Platform</span>
+        </h1>
+        <p className="mt-8 sm:mt-6 lg:mt-12">
+      Explore our services for resume building, <br/> interview preparation, and career counseling today
+      </p>
+      
+      {/* TODO */}
+      {/* Add three cards here */}
+      </div>
     </div>
   );
 }
--- engitrack/src/components/Footer.js
+++ engitrack/src/components/Footer.js
@@ -4,17 +4,17 @@ export default function Footer() {
         <div className="container mx-auto flex flex-col md:flex-row justify-between items-center pl-20 pr-20 pt-1">
           <p className="text-sm">&copy; {new Date().getFullYear()} EngiTrack. All rights reserved.</p>
           <div className="flex space-x-6 mt-4 md:mt-0">
-            <a href="https://twitter.com" aria-label="Twitter" target="_blank" rel="noopener noreferrer">
+            <a href="https://x.com/returnUsername" aria-label="Twitter" target="_blank" rel="noopener noreferrer">
               <svg className="w-6 h-6 fill-current text-black hover:text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M22.46 6c-.77.35-1.6.58-2.47.69a4.32 4.32 0 0 0 1.89-2.37 8.75 8.75 0 0 1-2.75 1.06A4.3 4.3 0 0 0 16.11 4c-2.4 0-4.35 1.95-4.35 4.35 0 .34.04.67.11.98A12.2 12.2 0 0 1 3.1 5.18a4.35 4.35 0 0 0-.59 2.19c0 1.52.77 2.86 1.93 3.65-.71-.02-1.38-.22-1.97-.54v.06c0 2.13 1.52 3.91 3.53 4.32a4.33 4.33 0 0 1-1.95.07c.55 1.72 2.14 2.97 4.02 3a8.69 8.69 0 0 1-5.38 1.85 12.26 12.26 0 0 0 6.65 1.95c7.98 0 12.35-6.61 12.35-12.35l-.01-.56A8.84 8.84 0 0 0 22.46 6z" />
               </svg>
             </a>
-            <a href="https://linkedin.com" aria-label="LinkedIn" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/ankit-orion/" aria-label="LinkedIn" target="_blank" rel="noopener noreferrer">
               <svg className="w-6 h-6 fill-current text-black hover:text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M19.2 3H4.8C4.36 3 4 3.36 4 3.8v16.4c0 .44.36.8.8.8h14.4c.44 0 .8-.36.8-.8V3.8c0-.44-.36-.8-.8-.8zM8.54 17.3H6.31V10.8h2.23v6.5zM7.42 9.68c-.7 0-1.28-.57-1.28-1.28 0-.7.57-1.28 1.28-1.28s1.28.57 1.28 1.28c0 .7-.57 1.28-1.28 1.28zM17.69 17.3h-2.23v-3.19c0-.76-.61-1.37-1.37-1.37-.75 0-1.37.61-1.37 1.37v3.19h-2.23V10.8h2.23v.99c.43-.67 1.19-1.14 2.02-1.14 1.43 0 2.6 1.17 2.6 2.6v4.05z" />
               </svg>
             </a>
-            <a href="https://github.com" aria-label="GitHub" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/ankit-orion" aria-label="GitHub" target="_blank" rel="noopener noreferrer">
               <svg className="w-6 h-6 fill-current text-black hover:text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.207 11.387.6.11.793-.26.793-.577v-2.165c-3.338.726-4.043-1.613-4.043-1.613-.547-1.387-1.334-1.757-1.334-1.757-1.09-.746.082-.73.082-.73 1.205.084 1.838 1.238 1.838 1.238 1.07 1.834 2.808 1.304 3.494.996.11-.776.417-1.304.76-1.603-2.665-.307-5.467-1.334-5.467-5.933 0-1.312.469-2.383 1.238-3.225-.123-.305-.537-1.537.117-3.202 0 0 1.01-.323 3.303 1.23a11.36 11.36 0 0 1 3.003-.404c1.018.004 2.046.137 3.003.404 2.292-1.553 3.302-1.23 3.302-1.23.655 1.665.241 2.897.118 3.202.769.842 1.238 1.913 1.238 3.225 0 4.61-2.806 5.622-5.48 5.923.43.372.812 1.102.812 2.22v3.293c0 .319.194.69.799.573C20.564 21.797 24 17.298 24 12 24 5.37 18.63 0 12 0z" />
               </svg>
